03ad8fa318141a5c96fff834b8f0d59898817e82
Christian Boitel <christian@iMac-Boitel.local>
2019-11-19 22:04:20 +0100

    Make openapi plugin operational

--- openapi/handler.go
+++ openapi/handler.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"html"
 	"net/http"
-	"os"
 
 	gologging "github.com/devopsfaith/krakend-gologging"
 )
@@ -23,7 +22,7 @@ func (r registerer) RegisterHandlers(f func(
 	f(string(r), r.registerHandlers)
 }
 
-func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, _ http.Handler) (http.Handler, error) {
+func (r registerer) registerHandlers(ctx context.Context, extra map[string]interface{}, originalHandler http.Handler) (http.Handler, error) {
 	fmt.Println("registerHandlers")
 	// check the passed configuration and initialize the plugin
 	name, ok := extra["name"].(string)
@@ -33,14 +32,15 @@ func (r registerer) registerHandlers(ctx context.Context, extra map[string]inter
 	if name != string(r) {
 		return nil, fmt.Errorf("unknown register %s", name)
 	}
-	logger, logger_err := gologging.NewLogger(extra, os.Stdout)
+	logger, logger_err := gologging.NewLogger(extra)
 	if logger_err != nil {
 		return nil, logger_err
 	}
 	// return the actual handler wrapping or your custom logic so it can be used as a replacement for the default http handler
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(req.URL.Path))
-		logger.Info("processed %q", html.EscapeString(req.URL.Path))
+		// fmt.Fprintf(w, "Hello, %q", html.EscapeString(req.URL.Path))
+		originalHandler.ServeHTTP(w, req)
+		logger.Info("processed", html.EscapeString(req.URL.Path))
 	}), nil
 }
 
