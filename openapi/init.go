a3653b6a3a8658c9cab65c066507073f6e878bec
Christian Boitel <cboitel@lfdj.com>
2019-11-20 16:30:44 +0100

    Add default logger to init

--- openapi/init.go
+++ openapi/init.go
@@ -1,12 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	gologging "github.com/devopsfaith/krakend-gologging"
+	"github.com/devopsfaith/krakend/logging"
+)
 
 // PluginVersion defined version of plugin
 var PluginVersion = "undefined"
 
+var defaultLogger logging.Logger
+
 func init() {
-	fmt.Printf("krakend-openapi handler plugin version %s loaded !", PluginVersion)
+	defaultExtra := map[string]interface{}{
+		"github_com/devopsfaith/krakend-gologging": map[string]interface{}{
+			"level":  "INFO",
+			"prefix": "[OPENAPI]",
+			"syslog": false,
+			"stdout": true,
+		},
+	}
+	initLogger, err := gologging.NewLogger(defaultExtra)
+	if err != nil {
+		panic(fmt.Sprintf("error initializing default logger for openapi plugin: %v", err))
+	}
+	defaultLogger = initLogger
+	defaultLogger.Info("krakend-openapi handler plugin version %s loaded !", PluginVersion)
 }
 
 func main() {}
